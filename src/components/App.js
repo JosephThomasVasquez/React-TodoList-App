b72764f6e3f292e49166a366b44b31efcc9e2a7d
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2020-03-18 22:40:53 -0700

    Initial commit, & Setup base file structure

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './Form';
+
+function App() {
+  return (
+    <div className="App pt-5">
+      <div className="container shadow">
+      <h1 className="title-main">Todo List</h1>
+      <Form />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Form() {
+    return (<form action="submit" className="user-form">
+        <input className="input-item" type="text" placeholder="Add Item"/>
+    </form>);
+}
+
+export default Form;
